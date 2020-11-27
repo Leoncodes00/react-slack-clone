f06edf264ce4c9bd8c7dc4b12c4f2a15030fa42f
Leoncodes00 <chachaiyang3@gmail.com>
2020-11-26 23:02:23 -0600

    changed api

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import "firebase/database";
 import "firebase/storage";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDYi7gNWV8SKyCRteDJJXYTskHuz8hEkFM",
-  authDomain: "react-slack-clone-55e67.firebaseapp.com",
-  databaseURL: "https://react-slack-clone-55e67.firebaseio.com",
-  projectId: "react-slack-clone-55e67",
-  storageBucket: "react-slack-clone-55e67.appspot.com",
-  messagingSenderId: "494596104068",
-  appId: "1:494596104068:web:e0523dbaca6f03d33647d1",
-  measurementId: "G-3SKQ0YLF6F"
+  apiKey: "AIzaSyAe7i3juGvBHgpGIv5Y9gXhpbzBRv5GFLM",
+  authDomain: "react-slack-clone-4010b.firebaseapp.com",
+  databaseURL: "https://react-slack-clone-4010b.firebaseio.com",
+  projectId: "react-slack-clone-4010b",
+  storageBucket: "react-slack-clone-4010b.appspot.com",
+  messagingSenderId: "478702109964",
+  appId: "1:478702109964:web:9f1d404694ac550fb63529",
+  measurementId: "G-R7YNY9YGRT",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
